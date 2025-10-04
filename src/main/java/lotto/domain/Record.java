f1dd28c72e8e952a53cbd4bbeeec0e8f2545edbd
ParkJiYeoung8297 <wldud8297@gmail.com>
2025-10-05 05:46:15 +0900

    feat (domain) : 수익률 계산 기능 추가

--- src/main/java/lotto/domain/Record.java
+++ src/main/java/lotto/domain/Record.java
@@ -1,12 +1,16 @@
 package lotto.domain;
 
+import lotto.controller.Message;
+
 import java.util.HashMap;
 
 public class Record {
     private HashMap<Rank,Integer> rankRecord;
+    private Integer totalReturn;
 
     public Record(){
         rankRecord=new HashMap<>();
+        totalReturn=0;
         for (Rank rank: Rank.values()){
             rankRecord.put(rank,0);
         }
@@ -16,10 +20,17 @@ public class Record {
         // rankString이 enum 과 일치하는지 예외처리 필요
         Rank rank=Rank.valueOf(rankString);
         rankRecord.put(rank,rankRecord.get(rank)+1);
+        totalReturn+=rank.getAmount();
     }
 
     public void printRecord(){
+        for (Rank rank: Rank.values()){
+            System.out.println(String.format(Message.LOTTO_RESULT_DETAIL,rank.getDescription(),rankRecord.get(rank)));
+        }
+    }
 
+    public String getReturnPercent(Integer price){
+        return String.format("%.1f",(totalReturn/ (double)price)*100);
     }
 }
 
