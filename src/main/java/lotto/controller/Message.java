0d41859f6cbab91cfae6c78d65aba1b90b63d18d
ParkJiYeoung8297 <wldud8297@gmail.com>
2025-10-05 04:41:23 +0900

    refactor (message) : 출력 메세지 추가

--- src/main/java/lotto/controller/Message.java
+++ src/main/java/lotto/controller/Message.java
@@ -2,7 +2,10 @@ package lotto.controller;
 
 public class Message {
     public static final String REQUEST_PRICE_TO_PAY="구입 금액을 입력해 주세요.";
-    public static final String PRINT_THE_NUMBER_OF_LOTTO="%d개를 입력했습니다.";
+    public static final String PRINT_THE_NUMBER_OF_LOTTO="%d개를 구매했습니다.";
     public static final String REQUEST_TARGET_NUMBER="당첨 번호를 입력해 주세요";
     public static final String REQUEST_BONUS_NUMBER="보너스 번호를 입력해 주세요";
+    public static final String LOTTO_RESULT="당첨 통계\n---";
+    public static final String LOTTO_RESULT_DETAIL="%s - %d개";
+    public static final String LOTTO_RESULT_TOTAL="총 수익률은 %f\\%입니다.";
 }
