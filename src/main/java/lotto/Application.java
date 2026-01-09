2ba79106a0b5f91feb1226bec77867e97dd7b922
ParkJiYeoung8297 <wldud8297@gmail.com>
2026-01-09 19:21:49 +0900

    3차 초기화

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,18 +1,7 @@
 package lotto;
 
-import lotto.controller.LottoController;
-import lotto.service.LottoService;
-
 public class Application {
-
     public static void main(String[] args) {
-        LottoService service = new LottoService();
-        LottoController controller = new LottoController(service);
-        try {
-            controller.run();
-        } catch (IllegalStateException e) {
-            System.out.println(e.getMessage());
-        }
+        // TODO: 프로그램 구현
     }
 }
-
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,9 +1,7 @@
 package lotto;
 
-import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 
 import java.util.List;
 
